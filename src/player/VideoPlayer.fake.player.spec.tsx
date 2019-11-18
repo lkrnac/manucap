6e755fa0102029415de004dbd083e4520a1eec95
Lubos Krnac <lubos.krnac@gmail.com>
2019-11-18 17:46:28 +0100

    Test time shifting and reading
    VTMS-2003

--- src/player/VideoPlayer.fake.player.spec.tsx
+++ src/player/VideoPlayer.fake.player.spec.tsx
@@ -8,11 +8,12 @@ import * as React from "react";
 // @ts-ignore - Doesn't have types definitions file
 import * as simulant from "simulant";
 import * as videojs from "video.js";
-// @ts-ignore - We are mocking this with jest
 import VideoPlayer from "./VideoPlayer";
 
 jest.mock("video.js");
-const oLetter = 79;
+const O_CHAR = 79;
+const LEFT = 37;
+const RIGHT = 39;
 
 enzyme.configure({ adapter: new Adapter() });
 
@@ -32,7 +33,7 @@ describe("VideoPlayer", () => {
         enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
 
         // WHEN
-        simulant.fire(document.documentElement, "keydown", { keyCode: oLetter, shiftKey: true, altKey: true });
+        simulant.fire(document.documentElement, "keydown", { keyCode: O_CHAR, shiftKey: true, altKey: true });
 
         // THEN
         expect(play).toBeCalled();
@@ -53,9 +54,93 @@ describe("VideoPlayer", () => {
         enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
 
         // WHEN
-        simulant.fire(document.documentElement, "keydown", { keyCode: oLetter, shiftKey: true, altKey: true });
+        simulant.fire(document.documentElement, "keydown", { keyCode: O_CHAR, shiftKey: true, altKey: true });
 
         // THEN
         expect(pause).toBeCalled();
+    });
+
+    it("shifts playback forward via keyboard shortcut", () => {
+        // GIVEN
+        const currentTime = jest.fn();
+        currentTime.mockReturnValueOnce(5);
+        const playerMock = {
+            currentTime,
+            el: () => ({ parentElement: undefined }),
+            height: jest.fn(),
+            width: jest.fn(),
+        };
+        // @ts-ignore - we are mocking the module
+        videojs.mockImplementationOnce(() => playerMock);
+        enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
+
+        // WHEN
+        simulant.fire(document.documentElement, "keydown", { keyCode: RIGHT, shiftKey: true, altKey: true });
+
+        // THEN
+        expect(currentTime).toBeCalledWith(6);
+    });
+
+    it("shifts playback backwards via keyboard shortcut", () => {
+        // GIVEN
+        const currentTime = jest.fn();
+        currentTime.mockReturnValueOnce(5);
+        const playerMock = {
+            currentTime,
+            el: () => ({ parentElement: undefined }),
+            height: jest.fn(),
+            width: jest.fn(),
+        };
+        // @ts-ignore - we are mocking the module
+        videojs.mockImplementationOnce(() => playerMock);
+        enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
+
+        // WHEN
+        simulant.fire(document.documentElement, "keydown", { keyCode: LEFT, shiftKey: true, altKey: true });
+
+        // THEN
+        expect(currentTime).toBeCalledWith(4);
+    });
+
+    it("returns currentTime", () => {
+        // GIVEN
+        const playerMock = {
+            currentTime: () => 5,
+            el: () => ({ parentElement: undefined }),
+            height: jest.fn(),
+            width: jest.fn(),
+        };
+        // @ts-ignore - we are mocking the module
+        videojs.mockImplementationOnce(() => playerMock);
+        const actualNode = enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
+        const component = actualNode.instance() as VideoPlayer;
+
+        // WHEN
+        const actualTime = component.getTime();
+
+        // THEN
+        expect(actualTime).toEqual(5000);
+    });
+
+    it("moves time", () => {
+        // GIVEN
+        const currentTime = jest.fn();
+        currentTime.mockReturnValueOnce(5);
+        const playerMock = {
+            currentTime,
+            el: () => ({ parentElement: undefined }),
+            height: jest.fn(),
+            width: jest.fn(),
+        };
+        // @ts-ignore - we are mocking the module
+        videojs.mockImplementationOnce(() => playerMock);
+        const actualNode = enzyme.mount(<VideoPlayer id="testvpid" poster="dummyPosterUrl" mp4="dummyMp4Url"/>);
+        const component = actualNode.instance() as VideoPlayer;
+
+        // WHEN
+        component.moveTime(5600);
+
+        // THEN
+        expect(currentTime).toBeCalledWith(5.6);
     });
 });
