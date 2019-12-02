8f7bb1f0587b2f0a12029f8f1761eeecec643e79
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-02 10:16:21 +0100

    Create Redux support for local testing
    VTMS-2131

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,25 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import styles from "./styles.css";
 import { ReactElement } from "react";
+import { createStore } from "redux";
+import { connect } from "react-redux";
+import subtitleEditReducers from "./reducers/sustitleEditReducer";
+import { TestAction } from "./reducers/testReducer";
+
+const store = createStore(subtitleEditReducers);
+store.subscribe(() => console.log(store.getState()));
 
 const TestApp = (): ReactElement => (
     <div className={styles.test}>
-        Example Component: lala
+        After clicking on button, Redux event should appear in console.
+        <button onClick={(): void => {store.dispatch({ type: "TEST_ACTION", testValue: "clicked"})}} >
+            Test Redux
+        </button>
     </div>
 );
 
+const mapStateToProps = (state: TestAction): object => ({ testValue: state.testValue });
+
 ReactDOM.render(<TestApp />, document.getElementById("root"));
 
-export default TestApp;
+export default connect(mapStateToProps, null)(TestApp);
---
+++ src/reducers/sustitleEditReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import testReducer from "./testReducer";
+
+const subtitleEditReducers = combineReducers({
+    testReducer
+});
+
+export default subtitleEditReducers;
