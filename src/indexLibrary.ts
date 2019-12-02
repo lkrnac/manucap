5d06aa25c66fcadf467f6df5a23669c8f878bb78
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-02 15:35:31 +0100

    Export VideoPlayer as default export
    For time being. We will need to export two components at min. in future.
    VTMS-2131

--- src/indexLibrary.ts
+++ src/indexLibrary.ts
@@ -1,1 +1,14 @@
-export * from "./player/VideoPlayer";
+import VideoPlayer from "./player/VideoPlayer";
+export default VideoPlayer;
+
+// export * from "./player/VideoPlayer";
+
+// declare namespace SubtitleEdit {
+//     export * from "./player/VideoPlayer";
+// }
+
+// const lala = () => { ggg: "ggg" };
+// export lala;
+//
+// export default SubtitleEdit;
+
