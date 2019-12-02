8c786b8a9936e426d8fdb1625c8c7650055bdf76
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-02 10:30:52 +0100

    Enhance JSDom initialization so bundle doesn't contain test global type
    VTMS-2131

--- src/initBrowserEnvironment.ts
+++ src/initBrowserEnvironment.ts
@@ -8,30 +8,16 @@ interface ResizableTestingWindow extends Window {
     innerHeight: number;
 }
 
-declare global {
-    // TODO not sure how to get rid of this ESLint suppression so far
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace NodeJS {
-        interface Global {
-            document: Document;
-            window: ResizableTestingWindow | null;
-            navigator: Navigator | null;
-            location: Location | null;
-        }
-    }
-}
-
 new JSDOM("<!doctype html><html lang=\"en\"><body><div id=\"root\"/></body></html>");
 
-const resizeEvent = new window.Event("resize");
 
-if (global.window) {
-    const window = global.window;
-    window.resizeTo = (width: number, height: number): void => {
-        window.innerWidth = width ||  window.innerWidth;
-        window.innerHeight = height ||  window.innerHeight;
-        window.dispatchEvent(resizeEvent);
-    };
-}
+// @ts-ignore We need to make window resizable for test purposes - kind of
+const window = global.window as ResizableTestingWindow;
+const resizeEvent = new Event("resize");
+window.resizeTo = (width: number, height: number): void => {
+    window.innerWidth = width ||  window.innerWidth;
+    window.innerHeight = height ||  window.innerHeight;
+    window.dispatchEvent(resizeEvent);
+};
 
 enzyme.configure({ adapter: new Adapter() });
