a204df8361f49434f21836bd3b2c05783e4c790d
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-09 13:26:33 +0100

    Fix EsLint issue
    VTMS-2131

--- src/player/textTrackOptionsConversion.ts
+++ src/player/textTrackOptionsConversion.ts
@@ -3,7 +3,7 @@ import videojs from "video.js";
 
 export const convertToTextTrackOptions = (track: Track): videojs.TextTrackOptions => (
     {
-        kind: track.type == "CAPTION" ? "captions" : "subtitles",
+        kind: track.type === "CAPTION" ? "captions" : "subtitles",
         mode: "showing",
         srclang: track.language.id,
         default: track.default,
