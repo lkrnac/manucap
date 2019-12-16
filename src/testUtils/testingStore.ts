75f1f6b15d919aab0e234e539d8f56de08e9660b
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-16 10:23:08 +0100

    Disable serializable check
    VTMS-2131

--- src/testUtils/testingStore.ts
+++ src/testUtils/testingStore.ts
@@ -2,7 +2,7 @@ import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import subtitleEditReducers from "../reducers/subtitleEditReducer";
 
-const middleware = [...getDefaultMiddleware(), thunk];
+const middleware = [...getDefaultMiddleware({ serializableCheck: false }), thunk];
 const testingStore = configureStore({ reducer: subtitleEditReducers, middleware });
 
 export default testingStore;
