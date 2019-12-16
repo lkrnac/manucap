528f206935121b17f1dda3b17281f89a0c061325
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-16 10:38:36 +0100

    Remove duplicate middleware
    It is added by default middlewares from redux-toolkit
    VTMS-2147

--- src/testUtils/testingStore.ts
+++ src/testUtils/testingStore.ts
@@ -1,8 +1,7 @@
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
 import subtitleEditReducers from "../reducers/subtitleEditReducer";
 
-const middleware = [...getDefaultMiddleware({ serializableCheck: false }), thunk];
+const middleware = [...getDefaultMiddleware({ serializableCheck: false })];
 const testingStore = configureStore({ reducer: subtitleEditReducers, middleware });
 
 export default testingStore;
