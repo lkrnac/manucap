decfd9567497c321beceebdd49c6fd5a6b11ef75
rpoet-ds <russ.poetker@dotsub.com>
2019-12-18 18:20:21 -0500

    Update TS model for header

--- src/player/model.ts
+++ src/player/model.ts
@@ -1,3 +1,4 @@
+
 export interface Language {
     readonly id: string;
     readonly name: string;
@@ -7,9 +8,24 @@ export interface TrackVersion {
     readonly cues: VTTCue[];
 }
 
+export interface TrackDescription {
+    readonly action: string;
+    readonly subject: string;
+}
+
+export interface TrackProgress {
+    unit: string;
+    percentage: string;
+}
+
 export interface Track {
     readonly currentVersion?: TrackVersion;
     readonly type: "CAPTION" | "TRANSLATION";
     readonly language: Language;
     readonly default: boolean;
+    readonly videoTitle: string;
+    readonly projectName: string;
+    readonly dueDate: string;
+    readonly description: TrackDescription;
+    readonly progress: TrackProgress;
 }
