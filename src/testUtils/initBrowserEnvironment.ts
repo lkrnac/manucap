0f22c01d8c4a97cdf3540df60fa908e6807bc2dd
Lubos Krnac <lubos.krnac@gmail.com>
2019-12-18 12:31:13 +0100

    Remove JSDOM global resize fake
    It is not needed anymore
    VTMS-2147

--- src/testUtils/initBrowserEnvironment.ts
+++ src/testUtils/initBrowserEnvironment.ts
@@ -2,22 +2,6 @@ import * as enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import {JSDOM} from "jsdom";
 
-// Following declarations are here to maintain type safety of JSON initialization
-interface ResizableTestingWindow extends Window {
-    innerWidth: number;
-    innerHeight: number;
-}
-
 new JSDOM("<!doctype html><html lang=\"en\"><body><div id=\"root\"/></body></html>");
-
-
-// @ts-ignore We need to make window resizable for test purposes - kind of
-const window = global.window as ResizableTestingWindow;
-const resizeEvent = new Event("resize");
-window.resizeTo = (width: number, height: number): void => {
-    window.innerWidth = width ||  window.innerWidth;
-    window.innerHeight = height ||  window.innerHeight;
-    window.dispatchEvent(resizeEvent);
-};
 
 enzyme.configure({ adapter: new Adapter() });
