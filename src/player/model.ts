6548796a7c8f3ff94ca34438536802f987ef0c7d
rpoet-ds <russ.poetker@dotsub.com>
2019-12-19 15:30:20 -0500

    Add Task type

--- src/player/model.ts
+++ src/player/model.ts
@@ -8,24 +8,17 @@ export interface TrackVersion {
     readonly cues: VTTCue[];
 }
 
-export interface TrackDescription {
-    readonly action: string;
-    readonly subject: string;
-}
-
-export interface TrackProgress {
-    unit: string;
-    percentage: string;
-}
-
 export interface Track {
     readonly currentVersion?: TrackVersion;
     readonly type: "CAPTION" | "TRANSLATION";
     readonly language: Language;
     readonly default: boolean;
     readonly videoTitle: string;
+    readonly sourceTrack?: Track;
+}
+
+export interface Task {
+    readonly type: "TASK_CAPTION" | "TASK_TRANSLATE" | "TASK_DIRECT_TRANSLATE" | "TASK_REVIEW";
     readonly projectName: string;
     readonly dueDate: string;
-    readonly description: TrackDescription;
-    readonly progress: TrackProgress;
 }
