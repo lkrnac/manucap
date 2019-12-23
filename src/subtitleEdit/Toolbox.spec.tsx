6af14cbfb04c5570f040cc2752923bc78352d818
rpoet-ds <russ.poetker@dotsub.com>
2019-12-23 12:01:21 -0500

    Add toolbox for buttons

---
+++ src/subtitleEdit/Toolbox.spec.tsx
@@ -0,0 +1,44 @@
+import "../testUtils/initBrowserEnvironment";
+
+import * as enzyme from "enzyme";
+import * as React from "react";
+import {Provider} from "react-redux";
+import testingStore from "../testUtils/testingStore";
+import Toolbox from "./Toolbox";
+import KeyboardShortcuts from "./KeyboardShortcuts";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+
+describe("Toolbox", () => {
+    it("renders", () => {
+        // GIVEN
+        const expectedNode = enzyme.mount(
+            <Accordion defaultActiveKey="0" style={{ marginTop: "10px"}}>
+                <Card>
+                    <Accordion.Toggle as={Card.Header} variant="link" eventKey="0">
+                        Toolbox
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            <ButtonToolbar>
+                                <KeyboardShortcuts />
+                            </ButtonToolbar>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+        );
+
+        // WHEN
+        const actualNode = enzyme.mount(
+            <Provider store={testingStore} >
+                <Toolbox />
+            </Provider>
+        );
+
+        // THEN
+        expect(actualNode.html())
+            .toEqual(expectedNode.html());
+    });
+});
---
+++ src/subtitleEdit/Toolbox.tsx
@@ -0,0 +1,28 @@
+import React, {
+    ReactElement
+} from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import KeyboardShortcuts from "./KeyboardShortcuts";
+
+const Toolbox = (): ReactElement => {
+    return (
+        <Accordion defaultActiveKey="0" style={{ marginTop: "10px"}}>
+            <Card>
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="0">
+                    Toolbox
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <ButtonToolbar>
+                            <KeyboardShortcuts />
+                        </ButtonToolbar>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    );
+};
+
+export default Toolbox;
