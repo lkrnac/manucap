33c8568c12a81587619dd6428402666bcccb869d
Lubos Krnac <lubos.krnac@gmail.com>
2020-01-03 15:20:58 +0100

    Change general layout to align header text with player
    VTMS-2185

--- src/subtitleEdit/SubtitleEdit.tsx
+++ src/subtitleEdit/SubtitleEdit.tsx
@@ -15,14 +15,14 @@ export interface Props {
 const SubtitleEdit = (props: Props): ReactElement => {
     const cues = useSelector((state: SubtitleEditState) => state.cues);
     return (
-        <div className="sbte-subtitle-edit"  style={{display: "flex", flexFlow: "column"}}>
+        <div className="sbte-subtitle-edit"  style={{display: "flex", flexFlow: "column", padding: "10px"}}>
             <SubtitleEditHeader />
             <div style={{display: "flex", height: "100%"}}>
-                <div style={{flex: "1 1 0", padding: "10px", display: "flex", flexFlow: "column"}}>
+                <div style={{flex: "1 1 0", display: "flex", flexFlow: "column", paddingRight: "10px"}}>
                     <EditingVideoPlayer mp4={props.mp4} poster={props.poster}/>
                     <Toolbox />
                 </div>
-                <div style={{flex: "1 1 0", display: "flex", flexDirection: "column", padding: "10px"}}>
+                <div style={{flex: "1 1 0", display: "flex", flexDirection: "column", paddingLeft: "10px"}}>
                     {
                         cues.map((cue: VTTCue, idx: number): ReactElement => <CueTextEditor key={idx} index={idx} cue={cue}/>)
                     }
