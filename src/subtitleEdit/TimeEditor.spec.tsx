26517daf61057dfec1e8354b3715b852d5ad4f41
Adolfo Vazquez <adolfo.software@gmail.com>
2020-01-08 17:10:05 -0600

    Milliseconds overflow to seconds and seconds overflow to minutes

--- src/subtitleEdit/TimeEditor.spec.tsx
+++ src/subtitleEdit/TimeEditor.spec.tsx
@@ -2,42 +2,226 @@ import "../testUtils/initBrowserEnvironment";
 
 import * as enzyme from "enzyme";
 import * as React from "react";
-import {Provider} from "react-redux";
-import testingStore from "../testUtils/testingStore";
 import TimeEditor from "./TimeEditor";
 
 describe("TimeEditor", () => {
     it("renders", () => {
         // GIVEN
         const expectedNode = enzyme.mount(
-            <div style={{display: "flex"}} className="sbte-time-editor">
+            <div id="test" style={{display: "flex"}} className="sbte-time-editor">
                 <div style={{flexFlow: "column"}}>
-                    <input type="text" className="sbte-time-editor-input"/>
+                    <input id="test-minutes" type="text" className="sbte-time-editor-input" value="000"
+                           onChange={(): void => {}}/>
                 </div>
                 <div style={{flexFlow: "column"}}>
                     <div style={{verticalAlign: "bottom", padding: "5px"}}><span>:</span></div>
                 </div>
                 <div style={{flexFlow: "column"}}>
-                    <input type="text" className="sbte-time-editor-input" style={{width: "30px"}}/>
+                    <input id="test-seconds" type="text" className="sbte-time-editor-input" style={{width: "30px"}} value="00"
+                           onChange={(): void => {}}/>
                 </div>
                 <div>
                     <div style={{verticalAlign: "bottom", padding: "5px"}}><span>.</span></div>
                 </div>
                 <div style={{flexFlow: "column"}}>
-                    <input type="text" className="sbte-time-editor-input"/>
+                    <input id="test-milliseconds" type="text" className="sbte-time-editor-input" value="000"
+                           onChange={(): void => {}}/>
                 </div>
             </div>
         );
 
         // WHEN
         const actualNode = enzyme.mount(
-            <Provider store={testingStore} >
-                <TimeEditor />
-            </Provider>
+            <TimeEditor id="test"/>
         );
 
         // THEN
-        expect(actualNode.html())
-            .toEqual(expectedNode.html());
+        expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+
+    it("renders with some values", () => {
+        // GIVEN
+        const expectedNode = enzyme.mount(
+            <div id="test" style={{display: "flex"}} className="sbte-time-editor">
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-minutes" type="text" className="sbte-time-editor-input" value="999"
+                           onChange={(): void => {}}/>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <div style={{verticalAlign: "bottom", padding: "5px"}}><span>:</span></div>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-seconds" type="text" className="sbte-time-editor-input" style={{width: "30px"}} value="00"
+                           onChange={(): void => {}}/>
+                </div>
+                <div>
+                    <div style={{verticalAlign: "bottom", padding: "5px"}}><span>.</span></div>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-milliseconds" type="text" className="sbte-time-editor-input" value="999"
+                           onChange={(): void => {}}/>
+                </div>
+            </div>
+        );
+
+        // WHEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" minutes="999" milliseconds="999" />
+        );
+
+        // THEN
+        expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+
+    it("renders with all values", () => {
+        // GIVEN
+        const expectedNode = enzyme.mount(
+            <div id="test" style={{display: "flex"}} className="sbte-time-editor">
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-minutes" type="text" className="sbte-time-editor-input" value="999"
+                           onChange={(): void => {}}/>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <div style={{verticalAlign: "bottom", padding: "5px"}}><span>:</span></div>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-seconds" type="text" className="sbte-time-editor-input" style={{width: "30px"}} value="59"
+                           onChange={(): void => {}}/>
+                </div>
+                <div>
+                    <div style={{verticalAlign: "bottom", padding: "5px"}}><span>.</span></div>
+                </div>
+                <div style={{flexFlow: "column"}}>
+                    <input id="test-milliseconds" type="text" className="sbte-time-editor-input" value="999"
+                           onChange={(): void => {}}/>
+                </div>
+            </div>
+        );
+
+        // WHEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" minutes="999" seconds="59" milliseconds="999" />
+        );
+
+        // THEN
+        expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+
+    it("inputs ignores non numeric characters", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" minutes="999" seconds="59" milliseconds="999" />
+        );
+
+        // WHEN
+        actualNode.find("#test-minutes").simulate("blur", { target: { value: "abc!e@#.$%^" } });
+        actualNode.find("#test-seconds").simulate("blur", { target: { value: "abc!e@#.$%^" } });
+        actualNode.find("#test-milliseconds").simulate("blur", { target: { value: "abc!e@#.$%^" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("000");
+        expect(actualNode.find("#test-seconds").props().value).toEqual("00");
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("000");
+    });
+
+    it("inputs pad with 0s", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" />
+        );
+
+        // WHEN
+        actualNode.find("#test-minutes").simulate("blur", { target: { value: "1" } });
+        actualNode.find("#test-seconds").simulate("blur", { target: { value: "2" } });
+        actualNode.find("#test-milliseconds").simulate("blur", { target: { value: "33" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("001");
+        expect(actualNode.find("#test-seconds").props().value).toEqual("02");
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("033");
+    });
+
+    it("max minutes is 999", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" />
+        );
+
+        // WHEN
+        actualNode.find("#test-minutes").simulate("blur", { target: { value: "999999" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("999");
+    });
+
+    it("max seconds is 59", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" />
+        );
+
+        // WHEN
+        actualNode.find("#test-seconds").simulate("blur", { target: { value: "65" } });
+
+        // THEN
+        expect(actualNode.find("#test-seconds").props().value).toEqual("05");
+    });
+
+    it("max milliseconds is 999", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" />
+        );
+
+        // WHEN
+        actualNode.find("#test-milliseconds").simulate("blur", { target: { value: "1150" } });
+
+        // THEN
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("150");
+    });
+
+    it("seconds overflow to minutes", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" minutes="5"/>
+        );
+
+        // WHEN
+        actualNode.find("#test-seconds").simulate("blur", { target: { value: "80" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("006");
+        expect(actualNode.find("#test-seconds").props().value).toEqual("20");
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("000");
+    });
+
+    it("milliseconds overflow to seconds", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" seconds="10"/>
+        );
+
+        // WHEN
+        actualNode.find("#test-milliseconds").simulate("blur", { target: { value: "1001" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("000");
+        expect(actualNode.find("#test-seconds").props().value).toEqual("11");
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("001");
+    });
+
+    it("cascade overflow from milliseconds to minutes", () => {
+        // GIVEN
+        const actualNode = enzyme.mount(
+            <TimeEditor id="test" minutes="20" seconds="59" />
+        );
+
+        // WHEN
+        actualNode.find("#test-milliseconds").simulate("blur", { target: { value: "3563" } });
+
+        // THEN
+        expect(actualNode.find("#test-minutes").props().value).toEqual("021");
+        expect(actualNode.find("#test-seconds").props().value).toEqual("02");
+        expect(actualNode.find("#test-milliseconds").props().value).toEqual("563");
     });
 });
--- src/subtitleEdit/TimeEditor.tsx
+++ src/subtitleEdit/TimeEditor.tsx
@@ -1,73 +1,83 @@
 import React, {
     ChangeEvent,
-    ReactElement
+    ReactElement, useState
 } from "react";
 
 const MAX_MINUTES = 999;
-const MAX_SECONDS = 60;
-const MAX_MILLISECONDS = 1000;
+const MAX_SECONDS = 59;
+const MAX_MILLISECONDS = 999;
 const MINUTES = "minutes";
 const SECONDS = "seconds";
 const MILLISECONDS = "milliseconds";
 
 const removeNonNumeric = (value: string): string => {
-    return value.replace(/\D/g,"");
+    return value.replace(/\D/g, "");
 };
 
-const adjustValue = (stringValue: string, type: string): string => {
-    let value = Number(stringValue);
-    switch (type) {
-        case MINUTES:
-            if (value > MAX_MINUTES) {
-                value = MAX_MINUTES;
-            }
-            break;
-        case SECONDS:
-            if (value > MAX_SECONDS) {
-                const minutes = Math.floor(value / 60);
-                const seconds = value - minutes * 60;
-                value = seconds;
-                // addMinutes(minutes);
-            }
-            break;
-        case MILLISECONDS:
-            if (value > MAX_MILLISECONDS) {
-                const seconds = Math.floor(value / 1000);
-                const milliseconds = value - seconds * 1000;
-                value = milliseconds;
-                if (seconds > MAX_SECONDS) {
-                    // const minutes = Math.floor(value / 60);
-                    // const seconds = value - minutes * 60;
-                    // addSeconds(seconds);
-                    // addMinutes(minutes);
-                }
-            }
-            break;
-    }
-    return value.toString();
-};
-
-const padWithZeros = (value: string, type: string): string => {
+const padWithZeros = (value: number, type: string): string => {
     let maxLength = 3;
     if (type === SECONDS) {
         maxLength = 2;
     }
-    return value.padStart(maxLength, "0");
+    return value.toString().padStart(maxLength, "0");
 };
 
-const handleChange = (e: ChangeEvent<HTMLInputElement>, type: string): void => {
-    let value = e.target.value;
-    value = removeNonNumeric(value);
-    value = adjustValue(value, type);
-    value = padWithZeros(value, type);
-    e.target.value = value;
-};
+interface Props {
+    id: string;
+    minutes?: string;
+    seconds?: string;
+    milliseconds?: string;
+}
 
-const TimeEditor = (): ReactElement => {
+const TimeEditor = (props: Props): ReactElement => {
+    const [minutes, setMinutes] = useState(props.minutes);
+    const [seconds, setSeconds] = useState(props.seconds);
+    const [milliseconds, setMilliseconds] = useState(props.milliseconds);
+
+    const adjustValue = (stringValue: string, type: string): string => {
+        let value = Number(stringValue);
+        switch (type) {
+            case MINUTES:
+                if (value > MAX_MINUTES) {
+                    value = MAX_MINUTES;
+                }
+                setMinutes(padWithZeros(value, MINUTES));
+                break;
+            case SECONDS:
+                if (value > MAX_SECONDS) {
+                    const plusMinutes = Math.floor(value / 60);
+                    value = value - plusMinutes * 60;
+                    const currentMinutes = Number(minutes) + plusMinutes;
+                    adjustValue(currentMinutes.toString(), MINUTES);
+                }
+                setSeconds(padWithZeros(value, SECONDS));
+                break;
+            case MILLISECONDS:
+                if (value > MAX_MILLISECONDS) {
+                    const plusSeconds = Math.floor(value / 1000);
+                    value = value - plusSeconds * 1000;
+                    const currentSeconds = Number(seconds) + plusSeconds;
+                    adjustValue(currentSeconds.toString(), SECONDS);
+                }
+                setMilliseconds(padWithZeros(value, MILLISECONDS));
+                break;
+        }
+        return value.toString();
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>, type: string): void => {
+        let value = e.target.value;
+        value = removeNonNumeric(value);
+        adjustValue(value, type);
+    };
+
     return (
-        <div style={{display: "flex"}} className="sbte-time-editor">
+        <div id={props.id} style={{display: "flex"}} className="sbte-time-editor">
             <div style={{flexFlow: "column"}}>
-                <input type="text" className="sbte-time-editor-input"
+                <input id={`${props.id}-minutes`} type="text" className="sbte-time-editor-input"
+                       value={minutes}
+                       onChange={(e): void => setMinutes(e.target.value)}
+                       onFocus={(e): void => e.target.select()}
                        onBlur={(e): void => handleChange(e, MINUTES)}
                 />
             </div>
@@ -75,7 +85,10 @@ const TimeEditor = (): ReactElement => {
                 <div style={{verticalAlign: "bottom", padding: "5px"}}><span>:</span></div>
             </div>
             <div style={{flexFlow: "column"}}>
-                <input type="text" className="sbte-time-editor-input" style={{width: "30px"}}
+                <input id={`${props.id}-seconds`} type="text" className="sbte-time-editor-input" style={{width: "30px"}}
+                       value={seconds}
+                       onChange={(e): void => setSeconds(e.target.value)}
+                       onFocus={(e): void => e.target.select()}
                        onBlur={(e): void => handleChange(e, SECONDS)}
                 />
             </div>
@@ -83,12 +96,21 @@ const TimeEditor = (): ReactElement => {
                 <div style={{verticalAlign: "bottom", padding: "5px"}}><span>.</span></div>
             </div>
             <div style={{flexFlow: "column"}}>
-                <input type="text" className="sbte-time-editor-input"
+                <input id={`${props.id}-milliseconds`} type="text" className="sbte-time-editor-input"
+                       value={milliseconds}
+                       onChange={(e): void => setMilliseconds(e.target.value)}
+                       onFocus={(e): void => e.target.select()}
                        onBlur={(e): void => handleChange(e, MILLISECONDS)}
                 />
             </div>
         </div>
     );
 };
+
+TimeEditor.defaultProps = {
+    minutes: "000",
+    seconds: "00",
+    milliseconds: "000",
+} as Partial<Props>;
 
 export default TimeEditor;
