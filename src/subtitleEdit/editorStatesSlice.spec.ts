6dc9c566c9c0690c6db4cc6dbf9909195baae71c
Lubos Krnac <lubos.krnac@gmail.com>
2020-01-10 09:31:01 +0100

    Use number as key in editor state maps
    Because we will be using cue index as this key
    VTMS-2197

--- src/subtitleEdit/editorStatesSlice.spec.ts
+++ src/subtitleEdit/editorStatesSlice.spec.ts
@@ -3,31 +3,29 @@ import testingStore from "../testUtils/testingStore";
 import {ContentState, EditorState} from "draft-js";
 
 describe("editorStatesSlice", () => {
-    it("updates editor state for 'editor1' ID", () => {
+    it("updates editor state for ID 1", () => {
         // GIVEN
         const contentState = ContentState.createFromText("editor1 text");
         const editorState = EditorState.createWithContent(contentState);
 
         // WHEN
-        testingStore.dispatch(updateEditorState("editor1", editorState));
+        testingStore.dispatch(updateEditorState(1, editorState));
 
         // THEN
         // @ts-ignore Test would fail if it returns null
-        expect(testingStore.getState().editorStates.get("editor1").getCurrentContent().getPlainText())
-            .toEqual("editor1 text");
+        expect(testingStore.getState().editorStates.get(1).getCurrentContent().getPlainText()).toEqual("editor1 text");
     });
 
-    it("updates editor state for 'editor2' ID", () => {
+    it("updates editor state for editor ID 2", () => {
         // GIVEN
         const contentState = ContentState.createFromText("editor2 text");
         const editorState = EditorState.createWithContent(contentState);
 
         // WHEN
-        testingStore.dispatch(updateEditorState("editor2", editorState));
+        testingStore.dispatch(updateEditorState(2, editorState));
 
         // THEN
         // @ts-ignore Test would fail if it returns null
-        expect(testingStore.getState().editorStates.get("editor2").getCurrentContent().getPlainText())
-            .toEqual("editor2 text");
+        expect(testingStore.getState().editorStates.get(2).getCurrentContent().getPlainText()).toEqual("editor2 text");
     });
 });
--- src/subtitleEdit/editorStatesSlice.ts
+++ src/subtitleEdit/editorStatesSlice.ts
@@ -4,20 +4,24 @@ import {AppThunk} from "../reducers/subtitleEditReducers";
 import {Dispatch} from "react";
 
 interface EditorStateAction {
-    editorId: string;
+    editorId: number;
     editorState: EditorState;
 }
 
 export const editorStatesSlice = createSlice({
     name: "editorStates",
-    initialState: new Map<string, EditorState>(),
+    initialState: new Map<number, EditorState>(),
     reducers: {
-        updateEditorState: (state, action: PayloadAction<EditorStateAction>): Map<string, EditorState> =>
-            state.set(action.payload.editorId, action.payload.editorState)
+        updateEditorState: (state, action: PayloadAction<EditorStateAction>): Map<number, EditorState> =>
+            state.set(action.payload.editorId, action.payload.editorState),
+        reset: (): Map<number, EditorState> => new Map<number, EditorState>()
     }
 });
 
-export const updateEditorState = (editorId: string, editorState: EditorState): AppThunk =>
+export const updateEditorState = (editorId: number, editorState: EditorState): AppThunk =>
     (dispatch: Dispatch<PayloadAction<EditorStateAction>>): void => {
         dispatch(editorStatesSlice.actions.updateEditorState({ editorId, editorState }));
     };
+
+export const reset = (): AppThunk => (dispatch: Dispatch<PayloadAction<undefined>>): void =>
+    dispatch(editorStatesSlice.actions.reset());
