f8e394477a435cc8f76d2c75284cdaf67074a08a
Lubos Krnac <lubos.krnac@gmail.com>
2020-01-13 12:17:26 +0100

    Add linting rule to enforce wrapping multiline JSX into braces
    VTMS-2197

--- src/index.tsx
+++ src/index.tsx
@@ -60,10 +60,10 @@ const TestApp = (): ReactElement => {
         );
     });
 
-    return <SubtitleEdit
+    return (<SubtitleEdit
         poster="http://dotsub-media-encoded.s3.amazonaws.com/media/4/7/thumb.jpg"
         mp4="http://dotsub-media-encoded.s3.amazonaws.com/1/14/14.mp4"
-           />;
+            />);
 };
 
 ReactDOM.render(
