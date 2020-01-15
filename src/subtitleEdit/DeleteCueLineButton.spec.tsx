1cc8095f3f41b6ceb352a7321eb0aa4226cc6ad4
rpoet-ds <russ.poetker@dotsub.com>
2020-01-15 15:47:58 -0500

    Add delete cue button

---
+++ src/subtitleEdit/DeleteCueLineButton.spec.tsx
@@ -0,0 +1,30 @@
+import "../testUtils/initBrowserEnvironment";
+import DeleteCueLineButton from "./DeleteCueLineButton";
+import { Provider } from "react-redux";
+import React from "react";
+import { mount } from "enzyme";
+import testingStore from "../testUtils/testingStore";
+
+/**
+ * On click actions are covered by CueTextEditor tests
+ */
+describe("DeleteCueLineButton", () => {
+    it("renders", () => {
+        // GIVEN
+        const expectedNode = mount(
+            <button className="btn btn-outline-secondary sbte-delete-cue-button">
+                <b>Delete</b>
+            </button>
+        );
+
+        // WHEN
+        const actualNode = mount(
+            <Provider store={testingStore}>
+                <DeleteCueLineButton cueIndex={0} />
+            </Provider>
+        );
+
+        // THEN
+        expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+});
