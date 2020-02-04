81dabeb490d45956a6955efa7ed1248ff6280d0e
Lubos Krnac <lubos.krnac@gmail.com>
2020-02-04 13:40:01 +0100

    Fix VideoPlayer paths after directory structure change

--- src/subtitleEdit/player/VideoPlayer.tsx
+++ src/subtitleEdit/player/VideoPlayer.tsx
@@ -1,12 +1,12 @@
 import "video.js/dist/video-js.css";
-import * as shortcuts from "../utils/shortcutConstants";
-import { CueDto, LanguageCues, Track } from "../subtitleEdit/model";
+import * as shortcuts from "../shortcutConstants";
+import { CueDto, LanguageCues, Track } from "../model";
 import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from "video.js";
 import Mousetrap from "mousetrap";
 import React from "react";
 import { ReactElement } from "react";
 import { convertToTextTrackOptions } from "./textTrackOptionsConversion";
-import { copyNonConstructorProperties } from "../subtitleEdit/cues/edit/cueUtils";
+import { copyNonConstructorProperties } from "../cues/edit/cueUtils";
 
 const SECOND = 1000;
 const PLAYBACK_RATES = [0.5, 0.75, 1, 1.25];
