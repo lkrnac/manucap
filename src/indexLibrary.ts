48af3402e0405e007c787e73748fb6fddfb71cfc
Russell Poetker <russ.poetker@dotsub.com>
2020-02-24 19:46:26 -0500

    Change css processing to avoid conflict during integration

--- src/indexLibrary.ts
+++ src/indexLibrary.ts
@@ -1,5 +1,12 @@
 import VideoPlayer from "./subtitleEdit/player/VideoPlayer";
+import { Reducers } from "./subtitleEdit/subtitleEditReducers";
+import { Actions } from "./subtitleEdit/trackSlices"
+import SubtitleEdit from "./subtitleEdit/SubtitleEdit";
 
-export default {
-    VideoPlayer: VideoPlayer
+export {
+    VideoPlayer,
+    Reducers,
+    SubtitleEdit,
+    Actions
 };
+
