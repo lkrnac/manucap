bda565f841d7ef0ca905d6a849c7dcfa803fa2eb
Lubos Krnac <lubos.krnac@gmail.com>
2020-02-26 09:56:44 +0100

    Simplify syntax
    VTMS-2213

--- src/subtitleEdit/cues/view/CueViewLine.tsx
+++ src/subtitleEdit/cues/view/CueViewLine.tsx
@@ -12,55 +12,52 @@ interface Props {
     playerTime: number;
 }
 
-const CueViewLine = (props: Props): ReactElement => {
-
-    return (
-        <>
+const CueViewLine = (props: Props): ReactElement => (
+    <>
+        <div
+            className="sbte-cue-line-left-section"
+            style={{
+                flex: "1 1 300px",
+                display: "flex",
+                flexDirection: "column",
+                paddingLeft: "10px",
+                paddingTop: "5px",
+                justifyContent: "space-between"
+            }}
+        >
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+            }}
+            >
+                <div>{getTimeString(props.cue.vttCue.startTime)}</div>
+                <div>{getTimeString(props.cue.vttCue.endTime)}</div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between", paddingBottom: "5px" }}>
+                <div className="sbte-small-font">{cueCategoryToPrettyName[props.cue.cueCategory]}</div>
+                <div className="sbte-small-font" style={{ paddingRight: "10px" }}>
+                    {findPositionIcon(props.cue.vttCue).iconText}
+                </div>
+            </div>
+        </div>
+        <div className="sbte-left-border" style={{ flex: "1 1 70%" }}>
             <div
-                className="sbte-cue-line-left-section"
+                className="sbte-cue-editor"
                 style={{
-                    flex: "1 1 300px",
-                    display: "flex",
-                    flexDirection: "column",
+                    flexBasis: "50%",
                     paddingLeft: "10px",
                     paddingTop: "5px",
-                    justifyContent: "space-between"
+                    paddingBottom: "5px",
+                    minHeight: "54px",
+                    height: "100%",
+                    width: "100%"
                 }}
-            >
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
+                dangerouslySetInnerHTML={{
+                    __html: convertVttToHtml(sanitizeHtml(props.cue.vttCue.text, { allowedTags: ["b", "i", "u"]}))
                 }}
-                >
-                    <div>{getTimeString(props.cue.vttCue.startTime)}</div>
-                    <div>{getTimeString(props.cue.vttCue.endTime)}</div>
-                </div>
-                <div style={{ display: "flex", justifyContent: "space-between", paddingBottom: "5px" }}>
-                    <div className="sbte-small-font">{cueCategoryToPrettyName[props.cue.cueCategory]}</div>
-                    <div className="sbte-small-font" style={{ paddingRight: "10px" }}>
-                        {findPositionIcon(props.cue.vttCue).iconText}
-                    </div>
-                </div>
-            </div>
-            <div className="sbte-left-border" style={{ flex: "1 1 70%" }}>
-                <div
-                    className="sbte-cue-editor"
-                    style={{
-                        flexBasis: "50%",
-                        paddingLeft: "10px",
-                        paddingTop: "5px",
-                        paddingBottom: "5px",
-                        minHeight: "54px",
-                        height: "100%",
-                        width: "100%"
-                    }}
-                    dangerouslySetInnerHTML={{
-                        __html: convertVttToHtml(sanitizeHtml(props.cue.vttCue.text, { allowedTags: ["b", "i", "u"]}))
-                    }}
-                />
-            </div>
-        </>
-    );
-};
+            />
+        </div>
+    </>
+);
 
 export default CueViewLine;
