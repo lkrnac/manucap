f3ccce5d15a74b3d2afae2e7d8e001a51eec87a1
Lubos Krnac <lubos.krnac@gmail.com>
2020-02-27 09:31:07 +0100

    Update cues for local testing
    VTMS-2213

--- src/index.tsx
+++ src/index.tsx
@@ -29,9 +29,9 @@ const TestApp = (): ReactElement => {
     useEffect(() => {
        setTimeout( // this simulates latency caused by server roundtrip
            dispatch(updateCues([
-               { vttCue: new VTTCue(0, 1, "<i>First <b>Line</b></i> 1\nWrapped text"), cueCategory: "DIALOGUE" },
+               { vttCue: new VTTCue(0, 1, "<i>Editing <b>Line</b></i> 1\nWrapped text"), cueCategory: "DIALOGUE" },
                {
-                   vttCue: new VTTCue(1, 2, "<i><lang en>Second</lang> <b>Line</b></i> 2\nWrapped text"),
+                   vttCue: new VTTCue(1, 2, "<i><lang en>Editing</lang> <b>Line</b></i> 2\nWrapped text"),
                    cueCategory: "ONSCREEN_TEXT"
                },
            ])),
@@ -41,12 +41,13 @@ const TestApp = (): ReactElement => {
     useEffect(() => {
         setTimeout( // this simulates latency caused by server roundtrip
             dispatch(updateSourceCues([
-                { vttCue: new VTTCue(0, 1, "<i>Caption <b>Line</b></i> 1\nWrapped text"), cueCategory: "DIALOGUE" },
+                { vttCue: new VTTCue(0, 1, "<i>Source <b>Line</b></i> 1\nWrapped text"), cueCategory: "DIALOGUE" },
                 {
-                    vttCue: new VTTCue(1, 2, "<i><lang en>Caption</lang> <b>Line</b></i> 2\nWrapped text"),
+                    vttCue: new VTTCue(1, 2, "<i><lang en>Source</lang> <b>Line</b></i> 2\nWrapped text"),
                     cueCategory: "ONSCREEN_TEXT"
                 },
-                { vttCue: new VTTCue(2, 3, "<i>Caption <b>Line</b></i> 3\nWrapped text"), cueCategory: "DIALOGUE" },
+                { vttCue: new VTTCue(2, 3, "<i>Source <b>Line</b></i> 3\nWrapped text"), cueCategory: "DIALOGUE" },
+                { vttCue: new VTTCue(3, 4, "<i>Source <b>Line</b></i> 4\nWrapped text"), cueCategory: "DIALOGUE" },
             ])),
             500
         );
