50f8e907440c8cbcc71ba6e5f87b9b2fe28d05cc
Lubos Krnac <lubos.krnac@gmail.com>
2020-03-04 13:05:13 +0100

    Output ESLint output for debugging purposes

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -4,8 +4,9 @@ GITHUB_TOKEN=$CD_GITHUB_TOKEN
 #check for ESLint violations
 if (grep '<error' build/eslint.xml 1> /dev/null 2>&1) then
   sumEslint=$(cat build/eslint.xml | grep -c '<error')
+  cat build/eslint.xml
   echo "ESLint found "${sumEslint}" issues!";
-  curl -H "Authorization: token $GITHUB_TOKEN" --request POST --data '{"state": "failure", "context": "travis-ci/ESLint", "description": "ESLint found '"${sumEslint}"' issues!"}' https://api.github.com/repos/dotsub/vtms-subtitle-edit-ui/statuses/${TRAVIS_COMMIT}
+  curl -H "Authorization: token $GITHUB_TOKEN" --request POST --data '{"state": "failure", "context": "travis-ci/ESLint", "description": "ESLint found '"${sumEslint}"' issues!"}' https://api.github.com/repos/dotsub/vtms-subtitle-edit-ui/statuses/${TRAVIS_COMMIT} > /dev/null
 else
-  curl -H "Authorization: token $GITHUB_TOKEN" --request POST --data '{"state": "success", "context": "travis-ci/ESLint", "description": "ESLint Passed"}' https://api.github.com/repos/dotsub/vtms-subtitle-edit-ui/statuses/${TRAVIS_COMMIT}
+  curl -H "Authorization: token $GITHUB_TOKEN" --request POST --data '{"state": "success", "context": "travis-ci/ESLint", "description": "ESLint Passed"}' https://api.github.com/repos/dotsub/vtms-subtitle-edit-ui/statuses/${TRAVIS_COMMIT} > /dev/null
 fi
