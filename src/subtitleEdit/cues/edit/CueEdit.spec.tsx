9ac4ef2b4de892619d4216cda9d40257e349f86a
Adolfo Vazquez <adolfo.software@gmail.com>
2020-03-10 11:44:09 -0600

    Remove max/min time properties and prevent end time from being greater than start time

--- src/subtitleEdit/cues/edit/CueEdit.spec.tsx
+++ src/subtitleEdit/cues/edit/CueEdit.spec.tsx
@@ -362,8 +362,8 @@ describe("CueEdit", () => {
             .simulate("change", { target: { value: "00:00:03.000", selectionEnd: 12 }});
 
         // THEN
-        expect(testingStore.getState().cues[0].vttCue.startTime).toEqual(1);
-        expect(testingStore.getState().cues[0].vttCue.endTime).toEqual(2);
+        expect(testingStore.getState().cues[0].vttCue.startTime).toEqual(3);
+        expect(testingStore.getState().cues[0].vttCue.endTime).toEqual(3.5);
     });
 
     it("prevents end time from being less than start time", () => {
@@ -380,6 +380,6 @@ describe("CueEdit", () => {
 
         // THEN
         expect(testingStore.getState().cues[0].vttCue.startTime).toEqual(1);
-        expect(testingStore.getState().cues[0].vttCue.endTime).toEqual(2);
+        expect(testingStore.getState().cues[0].vttCue.endTime).toEqual(1.5);
     });
 });
