f798434c5666f145bb2566924661f745abaee624
youans <youans.ezzat@gmail.com>
2020-03-14 02:45:44 +0200

    Fix flap counter style
    VTMS-2307
    
    Fix flap counter style

--- src/subtitleEdit/cues/CueLine.tsx
+++ src/subtitleEdit/cues/CueLine.tsx
@@ -20,7 +20,7 @@ const CueLine = (props: Props): ReactElement => {
     const translationCueClassName = props.cue ? "sbte-gray-100-background" : "sbte-gray-200-background";
     return (
         <div onClick={props.onClickHandler} style={{ display: "flex", paddingBottom: "5px", width: "100%" }}>
-            <div className="sbte-cue-line-flap" style={{ paddingLeft: "8px", paddingTop: "10px" }} >
+            <div className="sbte-cue-line-flap" >
                 {props.index + 1}
             </div>
             <div style={{ display: "flex", flexDirection:"column", width: "100%" }}>
