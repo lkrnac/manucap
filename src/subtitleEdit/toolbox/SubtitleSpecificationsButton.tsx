1391865bb850ffdb6742ed6d469729644140a6c8
youans <youans.ezzat@gmail.com>
2020-03-18 00:04:10 +0200

    Fixing some eslint
    DSD-386
    
    Fixing some eslint

--- src/subtitleEdit/toolbox/SubtitleSpecificationsButton.tsx
+++ src/subtitleEdit/toolbox/SubtitleSpecificationsButton.tsx
@@ -11,7 +11,7 @@ const SubtitleSpecificationsButton = (): ReactElement => {
     useEffect(
         () => {
             setShow(subtitleSpecifications != null
-                && (cues.length == 0 || cues.length == 1 && cues[0]?.vttCue.text === ""));
+                && (cues.length === 0 || (cues.length === 1 && cues[0]?.vttCue.text === "")));
         }, [subtitleSpecifications, cues]
     );
 
