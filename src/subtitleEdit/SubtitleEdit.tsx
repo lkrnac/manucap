5d1c27cbac9eb97cbf542a973710342a7dca5e7d
Russell Poetker <russ.poetker@dotsub.com>
2020-03-31 13:26:17 -0400

    Add todo comments

--- src/subtitleEdit/SubtitleEdit.tsx
+++ src/subtitleEdit/SubtitleEdit.tsx
@@ -14,6 +14,9 @@ import { Toast } from "react-bootstrap";
 import { setAutoSaveSuccess } from "./cues/edit/editorStatesSlice";
 import { enableMapSet } from "immer";
 
+// TODO: enableMapSet is needed to workaround draft-js type issue.
+//  https://github.com/DefinitelyTyped/DefinitelyTyped/issues/43426
+//  Can be removed once fixed.
 enableMapSet();
 
 const autoSaveTimeout = 10000;
