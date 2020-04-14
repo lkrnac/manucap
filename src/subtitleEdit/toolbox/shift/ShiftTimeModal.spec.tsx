1c03d84bd42f10711861ffb39ec40b065f1f8d1b
Adolfo Vazquez <adolfo.software@gmail.com>
2020-04-14 17:08:23 -0500

    Fix broken unit tests

--- src/subtitleEdit/toolbox/shift/ShiftTimeModal.spec.tsx
+++ src/subtitleEdit/toolbox/shift/ShiftTimeModal.spec.tsx
@@ -10,6 +10,11 @@ import { mount } from "enzyme";
 import sinon from "sinon";
 import testingStore from "../../../testUtils/testingStore";
 
+const testCues = [
+    { vttCue: new VTTCue(0, 1, "Caption Line 1"), cueCategory: "DIALOGUE" },
+    { vttCue: new VTTCue(1, 2, "Caption Line 2"), cueCategory: "DIALOGUE" },
+] as CueDto[];
+
 describe("ShiftTimesModal", () => {
     it("renders with error message", () => {
         // GIVEN
@@ -79,7 +84,6 @@ describe("ShiftTimesModal", () => {
             .toEqual(expectedNode.html());
     });
 
-
     const expectedNodeWithErrorMsg = mount(
         <Provider store={testingStore}>
             <div className="fade modal-backdrop show" />
@@ -135,11 +139,7 @@ describe("ShiftTimesModal", () => {
 
     it("renders error message and disable apply button if shift is not valid", () => {
         // GIVEN
-        const cues = [
-            { vttCue: new VTTCue(0, 1, "Caption Line 1"), cueCategory: "DIALOGUE" },
-            { vttCue: new VTTCue(1, 2, "Caption Line 2"), cueCategory: "DIALOGUE" },
-        ] as CueDto[];
-        testingStore.dispatch(updateCues(cues) as {} as AnyAction);
+        testingStore.dispatch(updateCues(testCues) as {} as AnyAction);
 
         // WHEN
         const actualNode = mount(
@@ -153,41 +153,34 @@ describe("ShiftTimesModal", () => {
         expect(actualNode.html()).toEqual(expectedNodeWithErrorMsg.html());
     });
 
-
-    it("Calls cuesSlice.applyShiftTime when click apply", () => {
+    it("Applies the shift time on form submit with a valid shift value", () => {
         // // GIVEN
-        const cues = [
-            { vttCue: new VTTCue(0, 1, "Caption Line 1"), cueCategory: "DIALOGUE" },
-            { vttCue: new VTTCue(1, 2, "Caption Line 2"), cueCategory: "DIALOGUE" },
-        ] as CueDto[];
-        testingStore.dispatch(updateCues(cues) as {} as AnyAction);
-
-        // WHEN
+        testingStore.dispatch(updateCues(testCues) as {} as AnyAction);
         const actualNode = mount(
             <Provider store={testingStore}>
-                <ShiftTimesModal show onClose={(): void => undefined} />
+                <ShiftTimesModal show onClose={jest.fn()} />
             </Provider >
         );
-        actualNode.find("input[type='number']").simulate("change", { target: { value: 1 }});
-        actualNode.find("button.dotsub-shift-modal-apply-button").simulate("click");
 
+        // WHEN
+        actualNode.find("input[type='number']").simulate("change", { target: { value: 1 }});
+        actualNode.find("form").simulate("submit");
 
         // THEN
-        // TODO: What the hack is this ???
-        // expect(applyShiftSpy).toBeCalled
+        expect(testingStore.getState().cues[0].vttCue.startTime).toEqual(1);
+        expect(testingStore.getState().cues[1].vttCue.startTime).toEqual(2);
     });
-
 
     it("Calls cancel when click apply", () => {
         // GIVEN
         const onClose = sinon.spy();
-
         const actualNode = mount(
             <Provider store={testingStore}>
                 <ShiftTimesModal show onClose={onClose} />
             </Provider >
         );
 
+        // WHEN
         actualNode.find(".btn.btn-secondary").simulate("click");
 
         // THEN
