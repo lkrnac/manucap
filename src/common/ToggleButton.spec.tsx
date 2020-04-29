a264b27a4368bc7ad073b7139ba74f3ce08e4390
Adolfo Vazquez <adolfo.software@gmail.com>
2020-04-29 12:19:27 -0500

    Add unit tests for toggle button component

--- src/common/ToggleButton.spec.tsx
+++ src/common/ToggleButton.spec.tsx
@@ -1,15 +1,13 @@
 import "../testUtils/initBrowserEnvironment";
-import { mount } from "enzyme";
-import React from "react";
+import { mount, shallow } from "enzyme";
+import React, { ReactElement } from "react";
 import ToggleButton from "./ToggleButton";
 
 describe("ToggleButton", () => {
     it("renders", () => {
         // GIVEN
-        const expectedNode = mount(
-            <button
-                type="button"
-            >
+        const expectedNode = shallow(
+            <button type="button">
                 Click me!
             </button>
         );
@@ -17,7 +15,7 @@ describe("ToggleButton", () => {
         // WHEN
         const actualNode = mount(
             <ToggleButton
-                render={() => (
+                render={(): ReactElement => (
                     <>Click me!</>
                 )}
             />
@@ -25,5 +23,63 @@ describe("ToggleButton", () => {
 
         // THEN
         expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+
+    it("renders with class", () => {
+        // GIVEN
+        const expectedNode = shallow(
+            <button type="button" className="btn-secondary">
+                Click me!
+            </button>
+        );
+
+        // WHEN
+        const actualNode = mount(
+            <ToggleButton
+                className="btn-secondary"
+                render={(): ReactElement => (
+                    <>Click me!</>
+                )}
+            />
+        );
+
+        // THEN
+        expect(actualNode.html()).toEqual(expectedNode.html());
+    });
+
+    it("calls onClick on click", () => {
+        // GIVEN
+        const mockOnClick = jest.fn();
+        const actualNode = mount(
+            <ToggleButton
+                onClick={mockOnClick}
+                className="btn-secondary"
+                render={(): ReactElement => (
+                    <>Click me!</>
+                )}
+            />
+        );
+
+        // WHEN
+        actualNode.find("ToggleButton").simulate("click");
+
+        // THEN
+        expect(mockOnClick).toBeCalled();
+    });
+
+    it("passes toggle state to render prop", () => {
+        // GIVEN
+        const render = jest.fn();
+        const actualNode = mount(
+            <ToggleButton
+                render={render}
+            />
+        );
+
+        // WHEN
+        actualNode.find("ToggleButton").simulate("click");
+
+        // THEN
+        expect(render).toHaveBeenCalledWith(true);
     });
 });
