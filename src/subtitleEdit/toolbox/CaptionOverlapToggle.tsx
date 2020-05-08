6404429b864c475b1d88c2ec8e9d1b6a539d0027
Adolfo Vazquez <adolfo.software@gmail.com>
2020-05-08 15:57:50 -0500

    Disable caption overlapping on component unmount

--- src/subtitleEdit/toolbox/CaptionOverlapToggle.tsx
+++ src/subtitleEdit/toolbox/CaptionOverlapToggle.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SubtitleEditState } from "../subtitleEditReducers";
 import ToggleButton from "../../common/ToggleButton";
@@ -7,6 +7,15 @@ import { setOverlapCaptions } from "../cues/cueSlices";
 export const CaptionOverlapToggle = (): ReactElement => {
     const dispatch = useDispatch();
     const overlapCaptions = useSelector((state: SubtitleEditState) => state.overlapCaptions);
+    useEffect(
+        () => {
+            return (): void => { // return means it will be executed during unmount
+                dispatch(setOverlapCaptions(false));
+            };
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [] // Run only once -> unmount
+    );
     return (
         <ToggleButton
             className="btn btn-secondary"
