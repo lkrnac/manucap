d2f072ee8cc1bb0e9c6e82ec23af96bef3ad8352
Adolfo Vazquez <adolfo.software@gmail.com>
2020-06-10 19:39:29 -0500

    Disable overlap button while saving

--- src/subtitleEdit/toolbox/CaptionOverlapToggle.tsx
+++ src/subtitleEdit/toolbox/CaptionOverlapToggle.tsx
@@ -5,15 +5,19 @@ import ToggleButton from "../../common/ToggleButton";
 import { updateEditingTrack } from "../trackSlices";
 import { Track } from "../model";
 import { updateCues } from "../cues/cueSlices";
+import { Constants } from "../constants";
 
 export const CaptionOverlapToggle = (): ReactElement => {
     const dispatch = useDispatch();
     const editingTrack = useSelector((state: SubtitleEditState) => state.editingTrack);
     const cues = useSelector((state: SubtitleEditState) => state.cues);
     const overlapEnabled = editingTrack?.overlapEnabled;
+    const saveStatus = useSelector((state: SubtitleEditState) => state.saveStatus);
+    const isPending = saveStatus === Constants.AUTO_SAVE_SAVING_CHANGES_MSG;
     return (
         <ToggleButton
             className="btn btn-secondary"
+            disabled={isPending}
             toggled={overlapEnabled}
             onClick={(): void => {
                 const track = {
