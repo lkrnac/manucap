a8493faf490918a2a15db322628b66c4438ca3fc
Adolfo Vazquez <adolfo.software@gmail.com>
2020-06-15 14:11:10 -0500

    Sync time codes from sourceCues to cues on sync cues button click

--- src/subtitleEdit/toolbox/SyncCuesButton.tsx
+++ src/subtitleEdit/toolbox/SyncCuesButton.tsx
@@ -1,10 +1,18 @@
 import React, { ReactElement } from "react";
+import { useDispatch } from "react-redux";
+import { syncCues } from "../cues/cueSlices";
+import { callSaveTrack } from "../cues/saveSlices";
 
 const SyncCuesButton = (): ReactElement => {
+    const dispatch = useDispatch();
     return (
         <button
             type="button"
             className="sbte-sync-cues-button btn btn-secondary"
+            onClick={(): void => {
+                dispatch(syncCues());
+                dispatch(callSaveTrack());
+            }}
         >
             <i className="fas fa-sync" /> Sync Cues
         </button>
