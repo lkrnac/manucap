ee41d40fab86ac046214f30cf65be4601316a8dc
youans <youans.ezzat@gmail.com>
2020-09-21 04:52:16 +0200

    Reverting master
    VTMS-2712
    
    Reverting master

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { Provider, useDispatch } from "react-redux";
 import React, { ReactElement, useEffect } from "react";
-
-import { updateCues } from "./subtitleEdit/cues/cueSlices";
+import { updateCues, updateSourceCues } from "./subtitleEdit/cues/cueSlices";
 import { updateEditingTrack, updateTask } from "./subtitleEdit/trackSlices";
 import { CueDto, Language } from "./subtitleEdit/model";
 import ReactDOM from "react-dom";
@@ -18,58 +17,57 @@ const TestApp = (): ReactElement => {
     // #############################################################################################
     // #################### Comment this out if you need to test Captioning mode ###################
     // #############################################################################################
-    // useEffect(() => {
-    //     const cues = [] as CueDto[];
-    //     for(let idx = 0; idx < 300; idx++) {
-    //         cues.push({
-    //             vttCue: new VTTCue(idx * 3, (idx + 1) * 3, `<i>Source <b>Line</b></i> ${idx + 1}\nWrapped tssext`),
-    //             cueCategory: "DIALOGUE"
-    //         });
-    //     }
-    //     setTimeout( // this simulates latency caused by server roundtrip
-    //
-    //         () => dispatch(updateSourceCues(cues)),
-    //         20
-    //     );
-    // });
+    useEffect(() => {
+        const cues = [] as CueDto[];
+        for(let idx = 0; idx < 9999; idx++) {
+            cues.push({
+                vttCue: new VTTCue(idx * 3, (idx + 1) * 3, `<i>Source <b>Line</b></i> ${idx + 1}\nWrapped text`),
+                cueCategory: "DIALOGUE"
+            });
+        }
+        setTimeout( // this simulates latency caused by server roundtrip
+
+            () => dispatch(updateSourceCues(cues)),
+            500
+        );
+    });
     // #############################################################################################
 
     useEffect(() => {
         setTimeout( // this simulates latency caused by server roundtrip
             () => dispatch(updateEditingTrack({
-                // type: "TRANSLATION",
-                type: "CAPTION", // ** Change track type to CAPTION
+                type: "TRANSLATION",
+                // type: "CAPTION", // ** Change track type to CAPTION
                 language: { id: "en-US", name: "English (US)" } as Language,
                 default: true,
                 mediaTitle: "This is the video title",
                 mediaLength: 4250,
-                progress: 50,
-                id: "0fd7af04-6c87-4793-8d66-fdb19b5fd04d"
+                progress: 50
             })),
-            20
+            500
         );
     });
     useEffect(() => {
         const cues = [] as CueDto[];
-        for(let idx = 0; idx < 300; idx++) {
+        for(let idx = 0; idx < 9999; idx++) {
             cues.push({
-                vttCue: new VTTCue(idx * 3, (idx + 1) * 3, `<i>Editing <b>Line</b></i> ${idx + 1}\nWrappxed tssext`),
+                vttCue: new VTTCue(idx * 3, (idx + 1) * 3, `<i>Editing <b>Line</b></i> ${idx + 1}\nWrapped text`),
                 cueCategory: "DIALOGUE"
             });
         }
         setTimeout( // this simulates latency caused by server roundtrip
-            () => dispatch(updateCues(cues)),
-            20
-        );
+           () => dispatch(updateCues(cues)),
+           500
+       );
     });
     useEffect(() => {
         setTimeout( // this simulates latency caused by server roundtrip
             () => dispatch(updateTask({
-                type: "TASK_CAPTION",
-                projectName: "Project One",
-                dueDate: "2019/12/30 10:00AM"
+               type: "TASK_CAPTION",
+               projectName: "Project One",
+               dueDate: "2019/12/30 10:00AM"
             })),
-            20
+            500
         );
     });
     useEffect(() => {
@@ -83,14 +81,14 @@ const TestApp = (): ReactElement => {
                 spokenAudio: false,
                 speakerIdentification: "NUMBERED",
                 dialogueStyle: "DOUBLE_CHEVRON",
-                maxLinesPerCaption: 8,
+                maxLinesPerCaption: 2,
                 maxCharactersPerLine: 30,
-                minCaptionDurationInMillis: 20,
+                minCaptionDurationInMillis: 500,
                 maxCaptionDurationInMillis: 4000,
                 comments: "Note",
                 mediaNotes: "Media notes, please click [here](https://dotsub.com)"
             })),
-            20
+            500
         );
     });
 
@@ -103,7 +101,7 @@ const TestApp = (): ReactElement => {
                 setTimeout(
                     () => {
                         dispatch(setAutoSaveSuccess(true));
-                    }, 20
+                    }, 500
                 );
                 return;
             }}
