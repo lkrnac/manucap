1e6f71cdcdb9626ae2551a86cd09b3d7b6809068
Lubos Krnac <lubos.krnac@gmail.com>
2021-07-30 11:23:22 +0200

    Add contingency cues at the bottom
    DSD-780

--- src/subtitleEdit/cues/cuesList/CuesList.tsx
+++ src/subtitleEdit/cues/cuesList/CuesList.tsx
@@ -34,10 +34,13 @@ const CuesList = (props: Props): ReactElement => {
     const pageIndex = previousNonNullFocusedCueIndex.current > 0
         ? Math.floor(previousNonNullFocusedCueIndex.current / DEFAULT_PAGE_SIZE)
         : 0;
+    const lastPageIndex = Math.floor(matchedCues.matchedCues.length / DEFAULT_PAGE_SIZE);
     const startIndex = pageIndex === 0
         ? 0
         : pageIndex * DEFAULT_PAGE_SIZE - 5;
-    const endIndex = (pageIndex + 1) * DEFAULT_PAGE_SIZE;
+    const endIndex = pageIndex === lastPageIndex
+        ? (pageIndex + 1) * DEFAULT_PAGE_SIZE
+        : (pageIndex + 1) * DEFAULT_PAGE_SIZE + 5;
     const [refs, setRefs] = useState([] as RefObject<HTMLDivElement>[]);
 
     useEffect(
