dfb69d9bea7a84646da1ef1e9de8f77666795d3d
Russell Poetker <russ.poetker@dotsub.com>
2021-10-15 14:55:30 -0400

    Fix linting issue

--- src/testUtils/initBrowserEnvironment.ts
+++ src/testUtils/initBrowserEnvironment.ts
@@ -1,4 +1,4 @@
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { JSDOM } from "jsdom";
 import { configure } from "enzyme";
 
