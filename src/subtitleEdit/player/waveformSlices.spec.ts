fa8d009699b0341658c89df73577616c119cb4e9
Adolfo Vazquez <adolfo.software@gmail.com>
2022-01-26 11:38:55 -0600

    Add missing unit test

---
+++ src/subtitleEdit/player/waveformSlices.spec.ts
@@ -0,0 +1,31 @@
+import { Track } from "../model";
+import { resetEditingTrack, updateEditingTrack } from "../trackSlices";
+import { AnyAction } from "@reduxjs/toolkit";
+import { createTestingStore } from "../../testUtils/testingStore";
+import deepFreeze from "deep-freeze";
+import { waveformVisibleSlice } from "./waveformSlices";
+
+const testingTrack = {
+    type: "CAPTION",
+    language: { id: "en-US" },
+    default: true,
+    mediaTitle: "This is the video title",
+    timecodesUnlocked: true
+} as Track;
+
+const testingStore = createTestingStore();
+deepFreeze(testingStore.getState());
+
+describe("waveformVisibleSlice", () => {
+    it("resetEditingTrack resets the waveformVisible flag ", () => {
+        // GIVEN
+        testingStore.dispatch(updateEditingTrack(testingTrack) as {} as AnyAction);
+        testingStore.dispatch(waveformVisibleSlice.actions.setWaveformVisible(false));
+
+        // WHEN
+        testingStore.dispatch(resetEditingTrack() as {} as AnyAction);
+
+        // THEN
+        expect(testingStore.getState().waveformVisible).toBeTruthy();
+    });
+});
