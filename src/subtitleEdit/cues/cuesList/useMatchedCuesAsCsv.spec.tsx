ba3da88bdc67f857c0e793967e3a7c8fb8ab79a6
Russell Poetker <russ.poetker@dotsub.com>
2022-06-14 11:49:56 -0400

    Add more test scenarios

--- src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.spec.tsx
+++ src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.spec.tsx
@@ -82,6 +82,36 @@ describe("useMatchedCuesAsCsv", () => {
         );
     });
 
+    it("returns CSV for 1-n with no match translation match cues", () => {
+        // GIVEN
+        const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
+            <Provider store={testingStore}>{children}</Provider>
+        );
+        const targetCues = [
+            { vttCue: new VTTCue(0, 1, "Target Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(1, 2, "Target Line 2"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(3, 4, "Target Line 3"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+        const sourceCues = [
+            { vttCue: new VTTCue(0, 3, "Source Line 1"), cueCategory: "DIALOGUE" }
+        ] as CueDto[];
+
+        testingStore.dispatch(updateCues(targetCues) as {} as AnyAction);
+        testingStore.dispatch(updateSourceCues(sourceCues) as {} as AnyAction);
+
+        // WHEN
+        const { result } = renderHook(() => useMatchedCuesAsCsv(), { wrapper });
+        const actualCuesCsv = result.current();
+
+        // THEN
+        expect(actualCuesCsv).toEqual(
+            "Source Start,Source End,Source Test,Target Start,Target End,Target Text\r\n" +
+            "00:00:00.000,00:00:03.000,\"Source Line 1\",00:00:00.000,00:00:01.000,\"Target Line 1\"\r\n" +
+            "00:00:00.000,00:00:03.000,\"Source Line 1\",00:00:01.000,00:00:02.000,\"Target Line 2\"\r\n" +
+            ",,,00:00:03.000,00:00:04.000,\"Target Line 3\""
+        );
+    });
+
     it("returns CSV for n-1 translation match cues", () => {
         // GIVEN
         const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
@@ -110,6 +140,70 @@ describe("useMatchedCuesAsCsv", () => {
             "00:00:00.000,00:00:01.000,\"Source Line 1\",00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
             "00:00:01.000,00:00:02.000,\"Source Line 2\",00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
             "00:00:02.000,00:00:03.000,\"Source Line 3\",00:00:02.000,00:00:03.000,\"Target Line 3\""
+        );
+    });
+
+    it("returns CSV for n-1 no match translation match cues", () => {
+        // GIVEN
+        const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
+            <Provider store={testingStore}>{children}</Provider>
+        );
+        const targetCues = [
+            { vttCue: new VTTCue(0, 1, "Target Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(1, 2, "Target Line 2"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+        const sourceCues = [
+            { vttCue: new VTTCue(0, 1, "Source Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(1, 2, "Source Line 2"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(2, 3, "Source Line 3"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+
+        testingStore.dispatch(updateCues(targetCues) as {} as AnyAction);
+        testingStore.dispatch(updateSourceCues(sourceCues) as {} as AnyAction);
+
+        // WHEN
+        const { result } = renderHook(() => useMatchedCuesAsCsv(), { wrapper });
+        const actualCuesCsv = result.current();
+
+        // THEN
+        expect(actualCuesCsv).toEqual(
+            "Source Start,Source End,Source Test,Target Start,Target End,Target Text\r\n" +
+            "00:00:00.000,00:00:01.000,\"Source Line 1\",00:00:00.000,00:00:01.000,\"Target Line 1\"\r\n" +
+            "00:00:01.000,00:00:02.000,\"Source Line 2\",00:00:01.000,00:00:02.000,\"Target Line 2\"\r\n" +
+            "00:00:02.000,00:00:03.000,\"Source Line 3\",,,"
+        );
+    });
+
+    it("returns CSV for no match translation match cues", () => {
+        // GIVEN
+        const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
+            <Provider store={testingStore}>{children}</Provider>
+        );
+        const targetCues = [
+            { vttCue: new VTTCue(4, 5, "Target Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(5, 6, "Target Line 2"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+        const sourceCues = [
+            { vttCue: new VTTCue(0, 1, "Source Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(1, 2, "Source Line 2"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(2, 3, "Source Line 3"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+
+        testingStore.dispatch(updateCues(targetCues) as {} as AnyAction);
+        testingStore.dispatch(updateSourceCues(sourceCues) as {} as AnyAction);
+
+        // WHEN
+        const { result } = renderHook(() => useMatchedCuesAsCsv(), { wrapper });
+        const actualCuesCsv = result.current();
+
+        // THEN
+        expect(actualCuesCsv).toEqual(
+            "Source Start,Source End,Source Test,Target Start,Target End,Target Text\r\n" +
+            "00:00:00.000,00:00:01.000,\"Source Line 1\",,,\r\n" +
+            "00:00:01.000,00:00:02.000,\"Source Line 2\",,,\r\n" +
+            "00:00:02.000,00:00:03.000,\"Source Line 3\",,,\r\n" +
+            ",,,00:00:04.000,00:00:05.000,\"Target Line 1\"\r\n" +
+            ",,,00:00:05.000,00:00:06.000,\"Target Line 2\""
         );
     });
 
