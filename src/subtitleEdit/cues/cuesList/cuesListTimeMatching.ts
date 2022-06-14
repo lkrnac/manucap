3e896940ddd3842fde4fe8460e787bb4b2ce1a6e
Russell Poetker <russ.poetker@dotsub.com>
2022-06-14 12:10:12 -0400

    Move csv generation function to hook

--- src/subtitleEdit/cues/cuesList/cuesListTimeMatching.ts
+++ src/subtitleEdit/cues/cuesList/cuesListTimeMatching.ts
@@ -1,8 +1,6 @@
-import { CueDto, CueDtoWithIndex, CueLineDto } from "../../model";
-import { getTimeString } from "../../utils/timeUtils";
+import { CueDto, CueLineDto } from "../../model";
 
 const OVERLAP_RATIO = 0.65;
-const CSV_HEADER = "Source Start,Source End,Source Test,Target Start,Target End,Target Text\r\n";
 
 export interface MatchedCuesWithEditingFocus {
     matchedCues: CueLineDto[];
@@ -153,27 +151,4 @@ export const matchCuesByTime = (
     }
     const matchedCues = Array.from(cuesMap.values());
     return { matchedCues, editingFocusIndex: indexes.editingFocus };
-};
-
-const encodeText = (value: string): string => `"${value.replace(/"/g, "\"\"")}"`;
-
-const getCueCsvArray = (cues: Array<CueDtoWithIndex> | undefined) =>
-    cues?.length ? cues.map(cueDto => [
-        `${getTimeString(cueDto.cue.vttCue.startTime)}`,
-        `${getTimeString(cueDto.cue.vttCue.endTime)}`,
-        encodeText(cueDto.cue.vttCue.text)
-    ]) : [["","",""]];
-
-const cartesian = (sourceArray: Array<Array<string>>, targetArray: Array<Array<string>>) =>
-    sourceArray.flatMap(source => targetArray.map(target => [source, target].flat()));
-
-export const matchedCuesToCsv = (matchedCues: Array<CueLineDto>): string => {
-    const result = matchedCues.map(cueLineDto => {
-            const sourceArray = getCueCsvArray(cueLineDto.sourceCues);
-            const targetArray = getCueCsvArray(cueLineDto.targetCues);
-            const output = cartesian(sourceArray, targetArray);
-            return output.map(lineArray => lineArray.join(","));
-        }
-    );
-    return `${CSV_HEADER}${result.flat().join("\r\n")}`;
 };
