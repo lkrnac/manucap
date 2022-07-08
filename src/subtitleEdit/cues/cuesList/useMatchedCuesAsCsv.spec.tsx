61457dbd7fe98f2c27c831175cdec7d8194d0b58
youans <youans.ezzat@gmail.com>
2022-07-08 23:22:05 +0200

    Merge master and bump up version
    DSD-960

--- src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.spec.tsx
+++ src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.spec.tsx
@@ -1,13 +1,14 @@
 import "../../../testUtils/initBrowserEnvironment";
 import { renderHook } from "@testing-library/react-hooks";
 import { AnyAction } from "@reduxjs/toolkit";
-import { CueDto } from "../../model";
+import { CueDto, Language, Track } from "../../model";
 import { updateCues } from "./cuesListActions";
 import { createTestingStore } from "../../../testUtils/testingStore";
 import { updateSourceCues } from "../view/sourceCueSlices";
 import useMatchedCuesAsCsv from "./useMatchedCuesAsCsv";
 import { ReactElement } from "react";
 import { Provider } from "react-redux";
+import { updateEditingTrack } from "../../trackSlices";
 
 let testingStore = createTestingStore();
 
@@ -140,6 +141,72 @@ describe("useMatchedCuesAsCsv", () => {
             "00:00:00.000,00:00:01.000,\"Source Line 1\",00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
             "00:00:01.000,00:00:02.000,\"Source Line 2\",00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
             "00:00:02.000,00:00:03.000,\"Source Line 3\",00:00:02.000,00:00:03.000,\"Target Line 3\""
+        );
+    });
+
+    it("returns CSV for caption match cues", () => {
+        // GIVEN
+        const testingTrack = {
+            type: "CAPTION",
+            language: { id: "en-US", name: "English (US)" } as Language,
+            default: true,
+            mediaTitle: "This is the video title",
+            mediaLength: 4000,
+            progress: 50
+        } as Track;
+        testingStore.dispatch(updateEditingTrack(testingTrack) as {} as AnyAction);
+        const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
+            <Provider store={testingStore}>{children}</Provider>
+        );
+        const targetCues = [
+            { vttCue: new VTTCue(0, 2, "Target Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(2, 3, "Target Line 3"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+
+        testingStore.dispatch(updateCues(targetCues) as {} as AnyAction);
+
+        // WHEN
+        const { result } = renderHook(() => useMatchedCuesAsCsv(), { wrapper });
+        const actualCuesCsv = result.current();
+
+        // THEN
+        expect(actualCuesCsv).toEqual(
+            "Target Start,Target End,Target Text\r\n" +
+            "00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
+            "00:00:02.000,00:00:03.000,\"Target Line 3\""
+        );
+    });
+
+    it("returns CSV for direct translation match cues", () => {
+        // GIVEN
+        const testingTrack = {
+            type: "TRANSLATION",
+            language: { id: "en-US", name: "English (US)" } as Language,
+            default: true,
+            mediaTitle: "This is the video title",
+            mediaLength: 4000,
+            progress: 50
+        } as Track;
+        testingStore.dispatch(updateEditingTrack(testingTrack) as {} as AnyAction);
+        const wrapper = ({ children }: WrapperProps): ReactElement => ( // eslint-disable-line react/prop-types
+            <Provider store={testingStore}>{children}</Provider>
+        );
+        const targetCues = [
+            { vttCue: new VTTCue(0, 2, "Target Line 1"), cueCategory: "DIALOGUE" },
+            { vttCue: new VTTCue(2, 3, "Target Line 3"), cueCategory: "DIALOGUE" },
+        ] as CueDto[];
+
+        testingStore.dispatch(updateCues(targetCues) as {} as AnyAction);
+
+        // WHEN
+        const { result } = renderHook(() => useMatchedCuesAsCsv(), { wrapper });
+        const actualCuesCsv = result.current();
+
+        // THEN
+        expect(actualCuesCsv).toEqual(
+            "Target Start,Target End,Target Text\r\n" +
+            "00:00:00.000,00:00:02.000,\"Target Line 1\"\r\n" +
+            "00:00:02.000,00:00:03.000,\"Target Line 3\""
         );
     });
 
--- src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.ts
+++ src/subtitleEdit/cues/cuesList/useMatchedCuesAsCsv.ts
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux";
 import { SubtitleEditState } from "../../subtitleEditReducers";
 import { CueDtoWithIndex, CueLineDto } from "../../model";
 import { getTimeString } from "../../utils/timeUtils";
+import { isDirectTranslationTrack } from "../../utils/subtitleEditUtils";
 
-const CSV_HEADER = "Source Start,Source End,Source Text,Target Start,Target End,Target Text\r\n";
+const CSV_HEADER_SOURCE_ONLY = "Target Start,Target End,Target Text\r\n";
+const CSV_HEADER_SOURCE_AND_TARGET = "Source Start,Source End,Source Text,Target Start,Target End,Target Text\r\n";
 
 const encodeText = (value: string): string => `"${value.replace(/"/g, "\"\"")}"`;
 
@@ -12,26 +14,31 @@ const getCueCsvArray = (cues: Array<CueDtoWithIndex> | undefined) =>
         `${getTimeString(cueDto.cue.vttCue.startTime)}`,
         `${getTimeString(cueDto.cue.vttCue.endTime)}`,
         encodeText(cueDto.cue.vttCue.text)
-    ]) : [["","",""]];
+    ]) : [["", "", ""]];
 
 const cartesian = (sourceArray: Array<Array<string>>, targetArray: Array<Array<string>>) =>
     sourceArray.flatMap(source => targetArray.map(target => [source, target].flat()));
 
-export const matchedCuesToCsv = (matchedCues: Array<CueLineDto>): string => {
+export const matchedCuesToCsv = (matchedCues: Array<CueLineDto>, isSourceOnlyTrack: boolean): string => {
     const result = matchedCues.map(cueLineDto => {
-            const sourceArray = getCueCsvArray(cueLineDto.sourceCues);
             const targetArray = getCueCsvArray(cueLineDto.targetCues);
-            const output = cartesian(sourceArray, targetArray);
+            let output = targetArray;
+            if (!isSourceOnlyTrack) {
+                const sourceArray = getCueCsvArray(cueLineDto.sourceCues);
+                output = cartesian(sourceArray, targetArray);
+            }
             return output.map(lineArray => lineArray.join(","));
         }
     );
-    return `${CSV_HEADER}${result.flat().join("\r\n")}`;
+    return `${isSourceOnlyTrack ? CSV_HEADER_SOURCE_ONLY : CSV_HEADER_SOURCE_AND_TARGET}${result.flat().join("\r\n")}`;
 };
 
 const useMatchedCuesAsCsv = (): Function => {
     const matchedCues = useSelector((state: SubtitleEditState) => state.matchedCues.matchedCues);
+    const track = useSelector((state: SubtitleEditState) => state.editingTrack);
     return () => {
-        return matchedCuesToCsv(matchedCues);
+        const isSourceOnlyTrack = track?.type === "CAPTION" || isDirectTranslationTrack(track);
+        return matchedCuesToCsv(matchedCues, isSourceOnlyTrack);
     };
 };
 
