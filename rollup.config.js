b151a603c7d016b5a26af5c983e6209568b613a3
Diego Balmaceda <diego@xaes.dev>
2023-01-19 15:54:25 -0600

    Changed rollup.config.js format.

--- rollup.config.js
+++ rollup.config.js
@@ -12,43 +12,41 @@ import pkg from "./package.json";
 import copy from "rollup-plugin-copy";
 
 export default {
-  input: 'src/indexLibrary.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    peerDepsExternal(),
-    postcss({
-      extract: path.resolve('dist/dist.css')
-    }),
-    url(),
-    json(),
-    svgr(),
-    resolve(),
-    typescript({
-      tsconfig: "./tsconfig.json"
-    }),
-    commonjs({
-      namedExports: {
-        'node_modules/humanize-duration/humanize-duration.js': ['humanizer'],
-        'node_modules//platform/platform.js': ['os'],
-        'node_modules/react-is/index.js': ['isValidElementType', 'isContextConsumer'],
-        'node_modules/draft-js/lib/Draft.js': ['SelectionState', 'EditorState', 'RichUtils', 'convertFromHTML',
-          'ContentState', 'Editor', 'getDefaultKeyBinding', 'CompositeDecorator', 'Modifier'
-        ],
-        'node_modules/lodash/lodash.js': ['lodash', 'debounce', 'findIndex', 'findLastIndex']
-      }
-    }),
-    copy({
-      targets: [{
-        src: "src/@types",
-        dest: "dist"
-      }]
-    })
-  ]
+    input: 'src/indexLibrary.ts',
+    output: [{
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+    }],
+    plugins: [
+        peerDepsExternal(),
+        postcss({
+            extract: path.resolve('dist/dist.css')
+        }),
+        url(),
+        json(),
+        svgr(),
+        resolve(),
+        typescript({
+            tsconfig: "./tsconfig.json"
+        }),
+        commonjs({
+            namedExports: {
+                'node_modules/humanize-duration/humanize-duration.js': ['humanizer'],
+                'node_modules//platform/platform.js': ['os'],
+                'node_modules/react-is/index.js': ['isValidElementType', 'isContextConsumer'],
+                'node_modules/draft-js/lib/Draft.js': ['SelectionState', 'EditorState', 'RichUtils', 'convertFromHTML',
+                    'ContentState', 'Editor', 'getDefaultKeyBinding', 'CompositeDecorator', 'Modifier'
+                ],
+                'node_modules/lodash/lodash.js': ['lodash', 'debounce', 'findIndex', 'findLastIndex']
+            }
+        }),
+        copy({
+            targets: [{
+                src: "src/@types",
+                dest: "dist"
+            }]
+        })
+    ]
 }
