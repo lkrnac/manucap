383e6645ec61f14ffad37f99e2012563069e0124
Adolfo Vazquez <adolfo.software@gmail.com>
2023-02-06 17:48:21 -0600

    Rollback previous change

--- src/subtitleEdit/cues/cuesList/cuesListActions.ts
+++ src/subtitleEdit/cues/cuesList/cuesListActions.ts
@@ -136,12 +136,10 @@ export const applySpellcheckerOnCue = createAsyncThunk(
             if (track && track.language?.id && spellCheckerSettings.enabled) {
                 return fetchSpellCheck(text, spellCheckerSettings, track.language.id)
                     .then(spellCheck => {
-                        if (spellCheck?.matches?.length > 0) {
-                            addSpellCheck(thunkApi.dispatch, index, spellCheck, track.id);
-                            const freshState: SubtitleEditState = thunkApi.getState() as SubtitleEditState;
-                            updateMatchedCue(thunkApi.dispatch, freshState, index);
-                            callSaveTrack(thunkApi.dispatch, thunkApi.getState);
-                        }
+                        addSpellCheck(thunkApi.dispatch, index, spellCheck, track.id);
+                        const freshState: SubtitleEditState = thunkApi.getState() as SubtitleEditState;
+                        updateMatchedCue(thunkApi.dispatch, freshState, index);
+                        callSaveTrack(thunkApi.dispatch, thunkApi.getState);
                     });
             }
         }
