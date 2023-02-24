0eb6e59973321895a0958d3a80ce6748112f934e
Russell Poetker <russ.poetker@dotsub.com>
2023-02-24 18:31:46 -0500

    Remove debounce from save track

--- src/subtitleEdit/cues/saveSlices.ts
+++ src/subtitleEdit/cues/saveSlices.ts
@@ -1,11 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { debounce } from "lodash";
 import { AppThunk } from "../subtitleEditReducers";
 import { Dispatch } from "react";
 import { CueDto, SubtitleEditAction, Track } from "../model";
 import { editingTrackSlice } from "../trackSlices";
 
-const DEBOUNCE_TIMEOUT = 2500;
 interface SaveActionWithPayload extends SubtitleEditAction {
     cues: CueDto[];
     editingTrack: Track | null;
@@ -34,8 +32,8 @@ export const setSaveTrack = (saveTrack: Function): AppThunk =>
         dispatch(saveTrackSlice.actions.set(saveTrack));
     };
 
-const sendSaveTrackRequest = (
-    dispatch: Dispatch<PayloadAction<boolean | SaveActionWithPayload>>,
+export const callSaveTrack = (
+    dispatch: Dispatch<PayloadAction<SubtitleEditAction>>,
     getState: Function
 ): void => {
     const cues = getState().cues;
@@ -45,13 +43,4 @@ const sendSaveTrackRequest = (
             { cues, editingTrack, shouldCreateNewVersion: true }
         ));
     }
-};
-
-const saveTrackDebounced = debounce(sendSaveTrackRequest, DEBOUNCE_TIMEOUT, { leading: false, trailing: true });
-
-export const callSaveTrack = (
-    dispatch: Dispatch<PayloadAction<SubtitleEditAction>>,
-    getState: Function
-): void => {
-    saveTrackDebounced(dispatch, getState);
 };
