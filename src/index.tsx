0a38f5e1ea4cbb5470c1d0dd00d1a675e513b518
Lubos Krnac <lubos.krnac@gmail.com>
2023-10-26 23:36:55 +0200

    Try to turn off fontawesome pro to solve Github actions issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import "@fortawesome/fontawesome-pro/css/fontawesome.min.css";
-import "@fortawesome/fontawesome-pro/css/duotone.min.css";
+// import "@fortawesome/fontawesome-pro/css/fontawesome.min.css";
+// import "@fortawesome/fontawesome-pro/css/duotone.min.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
