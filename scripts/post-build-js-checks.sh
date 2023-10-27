bf712a932ec25e3fa499b658aef0237f1e65d6c6
Lubos Krnac <lubos.krnac@gmail.com>
2023-10-27 02:16:03 +0200

    Try new version of logs parser

--- scripts/post-build-js-checks.sh
+++ scripts/post-build-js-checks.sh
@@ -11,47 +11,47 @@ fi
 # Check console outputs from JS test suite
 CONSOLE_PROBLEMS=$(cat */build/js-build.log | \
   # THESE ARE MOST WIDE SPREAD:
-  pcregrep -Mv 'console.error.*\n.*Warning: An update to.*inside a test'| \
-  pcregrep -Mv 'console.warn.*\n.*Warning: componentWillMount has been renamed' | \
-  pcregrep -Mv 'console.warn.*\n.*Warning: componentWillReceiveProps has been renamed' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: An update to.*inside a test'| \
+  pcre2grep -Mv 'console.warn.*\n.*Warning: componentWillMount has been renamed' | \
+  pcre2grep -Mv 'console.warn.*\n.*Warning: componentWillReceiveProps has been renamed' | \
 
-  pcregrep -Mv 'console.warn.*\n.*Warning: React.createFactory\(\) is deprecated' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Cannot update a component' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: React does not recognize the' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: You provided a' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: render\(\): Rendering components directly into document.body is discouraged' | \
-  pcregrep -Mv 'console.error.*\n.*Error: Error: connect' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Failed prop type:' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: render\(\.\.\.\): Replacing React-rendered children with a new root component' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: A component is changing a controlled input to be uncontrolled' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: A component is changing an uncontrolled input to be controlled' | \
-  pcregrep -Mv 'console.log.*\n.*base\.preprocess\(\) File' | \
-  pcregrep -Mv 'console.error.*\n.*Unexpected key ' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Expected `onClick` listener to be a function' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: useLayoutEffect does nothing on the server' | \
+  pcre2grep -Mv 'console.warn.*\n.*Warning: React.createFactory\(\) is deprecated' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Cannot update a component' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: React does not recognize the' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: You provided a' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: render\(\): Rendering components directly into document.body is discouraged' | \
+  pcre2grep -Mv 'console.error.*\n.*Error: Error: connect' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Failed prop type:' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: render\(\.\.\.\): Replacing React-rendered children with a new root component' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: A component is changing a controlled input to be uncontrolled' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: A component is changing an uncontrolled input to be controlled' | \
+  pcre2grep -Mv 'console.log.*\n.*base\.preprocess\(\) File' | \
+  pcre2grep -Mv 'console.error.*\n.*Unexpected key ' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Expected `onClick` listener to be a function' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: useLayoutEffect does nothing on the server' | \
 
-  pcregrep -Mv 'console.warn.*\n.*A response was recieved but no data entry was found' | \
-  pcregrep -Mv 'console.warn.*\n.*Please see https:' | \
+  pcre2grep -Mv 'console.warn.*\n.*A response was recieved but no data entry was found' | \
+  pcre2grep -Mv 'console.warn.*\n.*Please see https:' | \
 
-  pcregrep -Mv 'console.warn.*\n.*is missing in the.*defaultValue' | \
-  pcregrep -Mv 'console.warn.*\n.*Global fetch is deprecated and will be unsupported in a future version' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Received `false` for a non-boolean attribute' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Unknown event handler property' | \
-  pcregrep -Mv 'console.warn.*\n.*Deprecation warning:' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Each child in a list should have a unique' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: validateDOMNesting' | \
-  pcregrep -Mv 'console.warn.*\n.*Warning: You cannot change <Router history>' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: It looks like you.*re using the wrong act' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Failed context type: The context ' | \
-  pcregrep -Mv 'console.error.*\n.*Warning: Can.*t perform a React state update on an unmounted component' | \
+  pcre2grep -Mv 'console.warn.*\n.*is missing in the.*defaultValue' | \
+  pcre2grep -Mv 'console.warn.*\n.*Global fetch is deprecated and will be unsupported in a future version' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Received `false` for a non-boolean attribute' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Unknown event handler property' | \
+  pcre2grep -Mv 'console.warn.*\n.*Deprecation warning:' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Each child in a list should have a unique' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: validateDOMNesting' | \
+  pcre2grep -Mv 'console.warn.*\n.*Warning: You cannot change <Router history>' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: It looks like you.*re using the wrong act' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Failed context type: The context ' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: Can.*t perform a React state update on an unmounted component' | \
 
-  pcregrep -Mv 'console.error.*\n.*Warning: ErrorBoundary: Error boundaries should implement' | \
-  pcregrep -Mv 'console.log.*\n.*App error boundary Error: testing error boundary' | \
-  pcregrep -Mv 'console.error.*\n.*The above error occurred in the' | \
-  pcregrep -Mv 'console.error.*\n.*Error: Uncaught' | \
+  pcre2grep -Mv 'console.error.*\n.*Warning: ErrorBoundary: Error boundaries should implement' | \
+  pcre2grep -Mv 'console.log.*\n.*App error boundary Error: testing error boundary' | \
+  pcre2grep -Mv 'console.error.*\n.*The above error occurred in the' | \
+  pcre2grep -Mv 'console.error.*\n.*Error: Uncaught' | \
 
   # Remove `c` from following command to troubleshoot which issue was not caught by above suppressions
-  pcregrep -Mc '(\A\s*console\..*\n.*)|(Network Error)|(ECONNREFUSED)|(UnhandledPromiseRejectionWarning)')
+  pcre2grep -Mc '(\A\s*console\..*\n.*)|(Network Error)|(ECONNREFUSED)|(UnhandledPromiseRejectionWarning)')
 
 if (( CONSOLE_PROBLEMS > 0 )); then
   curl -H "Authorization: token $GITHUB_TOKEN" --request POST --data '{"state": "failure", "context": "travis-ci/console-issues", "description": "Found '"${CONSOLE_PROBLEMS}"' browser console issues!"}' https://api.github.com/repos/dotsub/manucap/statuses/${TRAVIS_COMMIT} > /dev/null
