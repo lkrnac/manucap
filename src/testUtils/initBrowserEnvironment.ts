a500ca1a33de9c1ef638d1b3eb72a87be91382d1
Lubos Krnac <lubos.krnac@gmail.com>
2025-02-02 13:46:39 +0100

    Fix missing global.fetch

--- src/testUtils/initBrowserEnvironment.ts
+++ src/testUtils/initBrowserEnvironment.ts
@@ -1,9 +1,11 @@
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { JSDOM } from "jsdom";
 import { configure } from "enzyme";
+import fetchMock from 'jest-fetch-mock';
 
 new JSDOM("<!doctype html><html lang=\"en\"><body><div id=\"root\"/></body></html>");
 
 configure({ adapter: new Adapter() });
 
 global.window.scrollTo = jest.fn();
+fetchMock.enableMocks();
