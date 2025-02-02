f94a577cf6631145ea0e03f1a893592e0374621f
Lubos Krnac <lubos.krnac@gmail.com>
2025-02-02 13:58:46 +0100

    Fix missing HTMLCanvasElement.getContext for test suite

--- src/testUtils/initBrowserEnvironment.ts
+++ src/testUtils/initBrowserEnvironment.ts
@@ -2,6 +2,16 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { JSDOM } from "jsdom";
 import { configure } from "enzyme";
 import fetchMock from 'jest-fetch-mock';
+import { createCanvas } from 'canvas';
+
+Object.defineProperty(global.HTMLCanvasElement.prototype, 'getContext', {
+    value: function (contextType: string) {
+        if (contextType === '2d') {
+            return createCanvas(300, 150).getContext('2d'); // Creates a mock 2D context
+        }
+        return null;
+    },
+});
 
 new JSDOM("<!doctype html><html lang=\"en\"><body><div id=\"root\"/></body></html>");
 
