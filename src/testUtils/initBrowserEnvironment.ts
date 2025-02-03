89b5ff4a71c5dc49877f587ed0c65c9ecd3fab3f
Lubos Krnac <lubos.krnac@gmail.com>
2025-02-03 21:13:43 +0100

    Try to fix coverage drop after Vite migration

--- src/testUtils/initBrowserEnvironment.ts
+++ src/testUtils/initBrowserEnvironment.ts
@@ -5,12 +5,7 @@ import fetchMock from 'jest-fetch-mock';
 import { createCanvas } from 'canvas';
 
 Object.defineProperty(global.HTMLCanvasElement.prototype, 'getContext', {
-    value: function (contextType: string) {
-        if (contextType === '2d') {
-            return createCanvas(300, 150).getContext('2d'); // Creates a mock 2D context
-        }
-        return null;
-    },
+    value: () =>  createCanvas(300, 150).getContext('2d')
 });
 
 new JSDOM("<!doctype html><html lang=\"en\"><body><div id=\"root\"/></body></html>");
